9676d5610e4b841d1477cc8ceb43650e62d53e45
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-12-15 10:30:43 +0300

    added env

---
+++ src/components/config/env.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+
+export const PhoneWidth = Dimensions.get('window').width;
+export const PhoneHeight = Dimensions.get('window').height
